92b73a6e45fa77e656b1d48e7745926a2889fd79
Noah Appel <noah.appel@lifelock.com>
2016-01-05 23:33:43 -0800

    Starter code for class 8

---
+++ class_starter_code/class_8_jquery/color_scheme_switcher/js/index.js
@@ -0,0 +1,12 @@
+document.querySelector('.grayButton').onclick = switchGray;
+document.querySelector('.whiteButton').onclick = switchWhite;
+
+function switchGray() {
+  document.body.style.backgroundColor = 'gray';
+  document.body.style.color = 'white';
+}
+
+function switchWhite() {
+  document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
+}
