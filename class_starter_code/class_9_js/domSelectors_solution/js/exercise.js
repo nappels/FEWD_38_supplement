92fbe08ff8fcefe340d970e379776cb17127d410
Noah Appel <noah.appel@lifelock.com>
2016-01-11 12:01:48 -0800

    Class 9 starter code.

---
+++ class_starter_code/class_9_js/domSelectors_solution/js/exercise.js
@@ -0,0 +1,24 @@
+/**
+ * Javascript/jQuery Exercises: Selecting Things In The DOM!
+ * Answer the following questions.
+ * Remember to
+ */
+
+
+// Question 1: When, button 1 is clicked, select all paragraphs on the page with jQuery, and changed the color to blue.
+
+
+
+// Question 2:When button 2 is clicked, select all <h2> elements on this page, and change the text to say your name.
+
+
+
+// Question 3:
+//  When button 3 is clicked, select the ordered list item that has the class "falseFact".
+//  Using jQuery change the content to read "True Fact".
+
+
+
+// Question 4:
+// Refactor question 3 keeping in mind separation of concerns
+
