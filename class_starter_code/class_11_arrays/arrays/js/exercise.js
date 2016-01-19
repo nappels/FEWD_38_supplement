588f87db2c62f8773fbdabb54cbd0e927091a554
Noah Appel <noah.appel@lifelock.com>
2016-01-18 17:56:56 -0800

    HW and starter code for class 11.

---
+++ class_starter_code/class_11_arrays/arrays/js/exercise.js
@@ -0,0 +1,31 @@
+/**
+ * Javascript Exercise: Data Types
+ * Most of your answers should be stored in variables called q1, q2 etc.. and the variables printed to the console.
+
+ 	(i.e) console.log("Question 1" + q1)
+ */
+
+/**
+ * Question 1
+ * Create an array of image sources. Use image1.png, image2.png, and image3.png as the array values.
+ */
+
+
+/**
+ * Question 2
+ * Using the array from Question 1, store the first element of the array in variable q2.
+ */
+
+
+/**
+ * Question 3
+ * Get the length of the first array (number of elements in the array) and store it in variable q3
+ */
+
+
+/**
+ * Question 4
+ *  Using the array from Question 1, store the last element of the array in variable q4.
+ *  Hint: How can we get the number of elements in the array?
+ */
+
---
+++ class_starter_code/class_11_arrays/carousel/js/index.js
@@ -0,0 +1,11 @@
+// Instructions:
+// In the images folder, you will find 8 food images (feel free to use other images if you want)
+// By default, food1.jpg is shown on the page.
+
+// 1) When a user clicks next, food2.jpg. If they click next again, food3.jpg should be shown and so on.
+
+// 2) When a user clicks back, you should show the previous image. So if you currently showing food3.jpg,
+// a click on previous would show food2.jpg. A click on previous again would show food1.jpg
+
+// Start with an array that contains strings of all the image paths.
+// You will also need a variable to keep track of which image is currently being shown to the user.
